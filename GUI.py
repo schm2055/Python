2d691aae323ddbd5a5b3264d323d384c60930199
Andrew Schmitt <dschm2055@gmail.com>
2016-09-04 21:08:25 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -4,7 +4,8 @@ from PyQt4 import QtGui, QtCore
 url = 'https://www.google.com'
 
 class Window(QtGui.QMainWindow):
-
+	
+	
     def __init__(self):
         super(Window, self).__init__()
         self.setGeometry(50, 50, 500, 300)
@@ -21,13 +22,17 @@ class Window(QtGui.QMainWindow):
 		#PRTG Button
         prtg_btn = QtGui.QPushButton("PRTG Web Console", self)
         prtg_btn.clicked.connect(self.prtg)
-        prtg_btn.resize(100,50)
+        prtg_btn.resize(150,50)
         prtg_btn.move(0,0)
 		#Show
         self.show()
+		
+	#url = 'https://www.google.com'
 	
     def prtg(self):
-        webbrowser(url, 2)
+        webbrowser.open(url,2)
+		
+	
         
 def run():
     app = QtGui.QApplication(sys.argv)
